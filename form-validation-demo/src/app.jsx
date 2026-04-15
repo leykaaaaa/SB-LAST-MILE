179504028e3114ee5297333abf4d0594a13c8915
gelii aceee <123482578+leykaaaaa@users.noreply.github.com>
2026-04-15 21:06:33 +0800

    Add files via upload

---
+++ form-validation-demo/src/app.jsx
@@ -0,0 +1,12 @@
+import Form from "./components/Form";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Form Validation Demo</h1>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
